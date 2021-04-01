545bb66a2c2c949d0e73121d2c5ee508946097b9
Avinash Reddy Sallagonda <40892223+Avinashreddy47@users.noreply.github.com>
2021-04-01 10:01:28 +0530

    Add files via upload

---
+++ application/logs/log-2021-03-30.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-03-30 15:27:32 --> Severity: Warning --> Undefined property: Image_controller::$db /var/www/html/Desktop/codeigniter-galleryproject/system/core/Model.php 73
+ERROR - 2021-03-30 15:27:32 --> Severity: error --> Exception: Call to a member function get_where() on null /var/www/html/Desktop/codeigniter-galleryproject/application/models/Image_model.php 14
+ERROR - 2021-03-30 15:27:32 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Desktop/codeigniter-galleryproject/system/core/Exceptions.php:271) /var/www/html/Desktop/codeigniter-galleryproject/system/core/Common.php 570
+ERROR - 2021-03-30 15:31:46 --> Severity: Warning --> Undefined array key "image" /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 20
+ERROR - 2021-03-30 15:31:46 --> Severity: Warning --> Trying to access array offset on value of type null /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 20
+ERROR - 2021-03-30 15:31:46 --> Severity: Warning --> Undefined array key "image" /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 21
+ERROR - 2021-03-30 15:31:46 --> Severity: Warning --> Trying to access array offset on value of type null /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 21
+ERROR - 2021-03-30 15:31:46 --> Severity: Warning --> Cannot modify header information - headers already sent by (output started at /var/www/html/Desktop/codeigniter-galleryproject/system/core/Exceptions.php:271) /var/www/html/Desktop/codeigniter-galleryproject/system/core/Common.php 570
---
+++ application/logs/log-2021-04-01.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-04-01 04:09:47 --> Severity: Warning --> Undefined array key "email" /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 25
+ERROR - 2021-04-01 04:09:47 --> Severity: Warning --> Array to string conversion /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 29
+ERROR - 2021-04-01 04:11:23 --> Severity: Warning --> Array to string conversion /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 29
+ERROR - 2021-04-01 04:12:17 --> Severity: Warning --> Array to string conversion /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 29
+ERROR - 2021-04-01 04:13:38 --> Severity: Warning --> Undefined array key "image" /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 34
+ERROR - 2021-04-01 04:13:38 --> Severity: Warning --> Trying to access array offset on value of type null /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 34
+ERROR - 2021-04-01 04:13:38 --> Severity: Warning --> Undefined array key "image" /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 35
+ERROR - 2021-04-01 04:13:38 --> Severity: Warning --> Trying to access array offset on value of type null /var/www/html/Desktop/codeigniter-galleryproject/application/controllers/Image_controller.php 35
